f745e362b421951b616cfd73f213d35f45ec5292
rick.chan <cy@haoan119.com>
2025-03-28 22:38:06 +0800

    Fix null termination on image frames.
    Signed-off-by: rick.chan <cy@haoan119.com>

--- v2/frame.go
+++ v2/frame.go
@@ -605,7 +605,7 @@ func (f ImageFrame) Bytes() []byte {
 		return bytes
 	}
 
-	if err = wr.WriteString(f.mimeType, encodedbytes.NativeEncoding); err != nil {
+	if err = wr.WriteNullTermString(f.mimeType, encodedbytes.NativeEncoding); err != nil {
 		return bytes
 	}
 
@@ -613,7 +613,7 @@ func (f ImageFrame) Bytes() []byte {
 		return bytes
 	}
 
-	if err = wr.WriteString(f.description, f.encoding); err != nil {
+	if err = wr.WriteNullTermString(f.description, f.encoding); err != nil {
 		return bytes
 	}
 
